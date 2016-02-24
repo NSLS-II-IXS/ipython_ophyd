069e8a37c05b8ca603818ac4f350fc1f01777998
Daron Chabot <dchabot@bnl.gov>
2016-02-24 10:06:22 -0500

    ENH: add bluesky olog integration

--- profile_collection/startup/00-startup.py
+++ profile_collection/startup/00-startup.py
@@ -6,6 +6,7 @@ from bluesky import qt_kicker
 
 qt_kicker.install_qt_kicker()
 RE=gs.RE
+RE.md['beamline_id'] = '10id'
 
 import matplotlib.pyplot as plt
 plt.ion()
@@ -20,5 +21,17 @@ from epics import caput, caget
 # ophyd expects to find 'logbook' in the IPython namespace
 from pyOlog import SimpleOlogClient
 logbook = SimpleOlogClient()
+
+from functools import partial
+from bluesky.callbacks.olog import logbook_cb_factory
+
+# Set up the logbook. This configures bluesky's summaries of
+# data acquisition (scan type, ID, etc.).
+
+logbook_func = logbook.log
+configured_logbook_func = partial(logbook_func, logbooks=['Data Acquisition'])
+
+cb = logbook_cb_factory(configured_logbook_func)
+RE.subscribe('start', cb)
 
 import ophyd
