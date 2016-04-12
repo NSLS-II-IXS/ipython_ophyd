2d1fb2857e33296277f8bc7fd105f0b3c5f5b0c6
Daron Chabot <dchabot@bnl.gov>
2016-04-12 12:28:55 -0400

    ENH: add current and position pvs to QuadEM

--- profile_collection/startup/95-detectors.py
+++ profile_collection/startup/95-detectors.py
@@ -57,6 +57,35 @@ class QuadEM(DetectorBase):
     trigger_mode = Cpt(EpicsSignal, 'TriggerMode')
     reset = Cpt(EpicsSignal, 'Reset')
 
+    current_name1 = Cpt(EpicsSignal, 'CurrentName1')
+    current_name2 = Cpt(EpicsSignal, 'CurrentName2')
+    current_name3 = Cpt(EpicsSignal, 'CurrentName3')
+    current_name4 = Cpt(EpicsSignal, 'CurrentName4')
+
+    current_offset1 = Cpt(EpicsSignal, 'CurrentOffset1')
+    current_offset2 = Cpt(EpicsSignal, 'CurrentOffset2')
+    current_offset3 = Cpt(EpicsSignal, 'CurrentOffset3')
+    current_offset4 = Cpt(EpicsSignal, 'CurrentOffset4')
+
+    current_offset_calc1 = Cpt(EpicsSignal, 'ComputeCurrentOffset1')
+    current_offset_calc2 = Cpt(EpicsSignal, 'ComputeCurrentOffset2')
+    current_offset_calc3 = Cpt(EpicsSignal, 'ComputeCurrentOffset3')
+    current_offset_calc4 = Cpt(EpicsSignal, 'ComputeCurrentOffset4')
+
+    current_scale1 = Cpt(EpicsSignal, 'CurrentScale1')
+    current_scale2 = Cpt(EpicsSignal, 'CurrentScale2')
+    current_scale3 = Cpt(EpicsSignal, 'CurrentScale3')
+    current_scale4 = Cpt(EpicsSignal, 'CurrentScale4')
+
+    position_offset_x = Cpt(EpicsSignal, 'PositionOffsetX')
+    position_offset_y = Cpt(EpicsSignal, 'PositionOffsetY')
+
+    position_offset_calc_x = Cpt(EpicsSignal, 'ComputePosOffsetX')
+    position_offset_calc_y = Cpt(EpicsSignal, 'ComputePosOffsetY')
+
+    position_scale_x = Cpt(EpicsSignal, 'PositionScaleX')
+    position_scale_Y = Cpt(EpicsSignal, 'PositionScaleY')
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
