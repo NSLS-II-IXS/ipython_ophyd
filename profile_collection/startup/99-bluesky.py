335e1e85c2418c1465632ddaf271718b840102bc
Daron Chabot <dchabot@bnl.gov>
2016-05-06 10:48:59 -0400

    Import DocumentToSpec from suitcase

--- profile_collection/startup/99-bluesky.py
+++ profile_collection/startup/99-bluesky.py
@@ -2,6 +2,7 @@ from bluesky.global_state import (resume, abort, stop, panic, all_is_well,
                                   state)
 
 from bluesky.callbacks.olog import OlogCallback
+from bluesky.callbacks import LivePlot
 from bluesky.global_state import gs
 
 
@@ -15,3 +16,13 @@ gs.DETS.append(det4)
 from bluesky.plans import *
 
 from bluesky.spec_api import ct, ascan, dscan
+
+
+from suitcase.spec import DocumentToSpec
+
+spec_cb = DocumentToSpec('/IXS/specfiles/spec0.spec')
+
+for spec_scan in [ascan, dscan, ct]:
+    spec_scan.subs['all'].append(spec_cb)
+
+#relabel_motors()
